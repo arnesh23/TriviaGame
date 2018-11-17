8e18b54ccb9dd82cd7e756a82ed986eb964fa1ab
Arnesh Regmi <arneshregmi@Arneshs-MacBook-Pro.local>
2018-11-17 00:08:10 -0800

    HW5

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//setTimeout(tenSeconds, 1000 * 1);
+
+
+
+setTimeout(tenSeconds, 1000 * 1);
+setTimeout(tenSeconds, 1000 * 5);
+
+
+function tenSeconds() {
+    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+    // console log 10 seconds left
+
+    $("#time-left").append("<h2>About 1 Seconds Left!</h2>");
+    console.log("1 seconds left");
+  }
+  
+  /*var QandA = [
+      {"Which of the following vegetables is not one of the ingredients of V-8 juice?": 
+      "A. Beet", "B. Carrot", "C. Spinach", "D. Cabbage"},
+      {"What is the main ingredient in vichyssoise?": 
+        "A. lima beans", "B. clams", "C. tomatoes", "D. potatoes"}
+      {"What country produces the most potatoes?": 
+        "A. China","B. United States", "C. Ireland", "D. Russia"},
+      {"What soft-drink company introduced the brand Slice?": 
+        "A. Dr. Pepper", "B. Coca Cola", "C. Seven Up", "D. Pepsico"},
+      {"According to a 1980s Beverage Media poll of four hundred bartenders, what is the average male customers favorite drink?":
+       "A. Beer", "B. Bourbon", "C. Scotch", "D. vodka"},
+      {"Simplesse is NutraSweet's fat substitue. What is it made of?":
+        "A. a blend of proteins from egg white and milk","fat molecules altered to be too large to digest", "C. molecules that are the mirror-image of normal fat molecules"},
+      {"Which grade of olive oil is considered the best?"
+        "A. extra virgin", "B. pure virgin", "C. superfine virgin"},
+      {"What vegetable has varities known as Bell Tower, Orobelle, and Jupiter?"},
+      "A. Onion", "B. Pepper", "C. Squash"
+      {"In the drink called zombie, what is the main alcoholic ingredient?"},
+      "A. Beer", "B. Brandy", "C. Rum", "D. Whiskey"}
+      
+
+  ]*/
